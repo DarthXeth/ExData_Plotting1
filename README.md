15b5c66b5d9e94552413203651d41f70c6a86865
DarthXeth <seth.waxman76@gmail.com>
2014-07-09 12:02:20 -0600

    Take Two at Assignment
    Finished up plot1.R by scrapping the idea of a common download/dataset
    loader script. R makes it hard to separate common tasks (or at least
    doesn't make it intuitive), so I removed initdata.R altogether.

--- README.md
+++ README.md
@@ -45,6 +45,7 @@ When loading the dataset into R, consider the following:
 * The dataset has 2,075,259 rows and 9 columns. It is not a small amount of data.
 * Only data from the dates 2007-02-01 and 2007-02-02 is used. 
 * I decided to try to read in only those dates rather than subsetting the entire dataset
+* I changed my mind above. I'll f with that some other assignment.
 
 * Obviously it was indeed useful to convert the Date and Time variables to
 Date/Time classes in R using the `strptime()` and `as.Date()`
--- plot1.R
+++ plot1.R
@@ -1,12 +1,37 @@
-# Author: Seth Waxman
-# Date Created: 8 Jul 2014
-#
-# The intent of plot1.r is to recreate the original plot1.png file
-# in Project 1 of Coursera's 'Exploratory Data Analysis' course
-# If you don't know what the above references but you've gotten this far, 
-# I'm sure you'll be able to figure it out
+# Data URL:
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# This script attempts to recreate the plot1.png sourced at:
+# https://github.com/rdpeng/ExData_Plotting1/raw/master/figure/unnamed-chunk-2.png
 
-# OK, I have to specifically download and load the data rather than just
-# assume the user will put it in the directory I want. Thanks for that. OK:
+# Load and filter household power consumption dataset
+LoadData <- function(file) 
+{  
+  # Just read the whole file and subset. remember to set the NA variable
+  powerData <- read.table(file, sep=";", header=T, na.strings="?")
+  
+  # Create a new "datetime" column by combining the Date and Time columns
+  powerData$datetime <- strptime(paste(powerData$Date, powerData$Time), "%d/%m/%Y %H:%M:%S")
+  
+  # Return only the subset of dates we are interested in as given
+  # in the assignment description
+  dates <- c(as.Date("2007-02-01"), as.Date("2007-02-02"))
+  powerData[as.Date(powerData$datetime) %in% dates,]
+}
 
-# Download and load data
+
+# Red histogram of global active power with modified x-axis label and title, default y-label, no legend
+plotIt <- function(dataset) 
+{
+  hist(dataset$Global_active_power, 
+       col = "red",
+       main = "Global Active Power",
+       xlab = "Global Active Power (kilowatts)")
+  
+}
+
+powerData <- LoadData("./household_power_consumption.txt")
+
+png("plot1.png", width=480, height=480)
+plotIt(powerData)
+
+dev.off()
